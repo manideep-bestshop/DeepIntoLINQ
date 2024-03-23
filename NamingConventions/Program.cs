df46aab8c1825f94b9da11f41f223d32c5e2516d
Admin <Admin@DESKTOP-7T0EOMO>
2024-03-23 18:16:42 +0530

    Naming Conventions

---
+++ NamingConventions/Program.cs
@@ -0,0 +1,61 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace NamingConventions
+{
+
+    //Naming Conventions
+    //    PascalCase
+    //    CamelCase
+    //    UPPERCASE
+    //    Prefixes
+    //    Plurals
+    //    Verb-Noun
+
+    class MyClass // Class-PascalCase
+    {
+        public string MyProperty {  get; set; } // Property-PascalCase
+
+        private int myAge; //Private Variable-CamelCase
+
+        public MyClass(string propertyValue) 
+        {
+            MyProperty= propertyValue;
+        }
+        public void Print(string contextText) // Parameter- CamelCase
+        {
+            string myText = ""; //Local Variable-CamelCase
+    i        Console.WriteLine(myText);
+        }
+        public static int AddNumbers(int a,int b)  // Method-PascalCase
+        {
+            return a + b;
+        }
+
+        interface IPrintImplements // interface- Prefixes
+        {
+            void Print(string contextText);
+        }
+        List<string> fruits = new List<string>(); // List-Plural
+
+        public void OnClick()  // Method- Verb-Noun
+        {
+
+        }
+        public string GetResults(int input)
+        {
+            int result = input * 2;
+            return result + " ";
+        }
+    }
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
